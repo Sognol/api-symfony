e009567964bc286cd29548b910fbed72230d382c
sognol <sognol.developer@gmail.com>
2022-04-21 14:04:53 +0200

    first commit

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    /**
+     * @Route("/library/list", name="library_list")
+     */
+    public function list(Request $request, LoggerInterface $logger) {
+        $response = new JsonResponse();
+        $response->setData([
+             [
+                 'id' => 1,
+                 'title' => 'Batman'
+             ],
+             [
+                 'id' => 2,
+                 'title' => 'El señor de los anillos'
+             ],
+             [
+                 'id' => 3,
+                 'title' => 'Los Vengadores'
+             ]
+        ]);
+        return $response;
+    }
+}
